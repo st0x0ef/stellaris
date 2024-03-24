0d33d6c83f1bd5e60553b658ceffe12a919ed8a3
TATHAN <costa.tathan@gmail.com>
2024-03-24 15:32:45 +0100

    Custom Config !

--- common/src/main/java/com/st0x0ef/stellaris/common/config/ConfigEntry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/config/ConfigEntry.java
@@ -1,100 +1,21 @@
 package com.st0x0ef.stellaris.common.config;
 
-import net.minecraft.util.StringRepresentable;
-
 import java.io.Serializable;
 
-public abstract class ConfigEntry implements Serializable {
+public class ConfigEntry<T> implements Serializable {
+    private T value;
 
-    public abstract ConfigType getType();
-
-    private static Boolean boolName;
-    private static int intName;
-    private static String stringName;
-    private static String description;
-
-    public ConfigEntry(String name, String description) {
-        this.stringName = name;
-        this.description = description;
-    }
-    public ConfigEntry(int name, String description) {
-        this.intName = name;
-        this.description = description;
-    }
-    public ConfigEntry(Boolean name, String description) {
-        this.boolName = name;
+    private String description;
+    public ConfigEntry(T value, String description) {
+        this.value = value;
         this.description = description;
     }
 
-    public String getName() {
-        return stringName;
+    public T getValue() {
+        return value;
     }
 
     public String getDescription() {
         return description;
     }
-
-    public static class ConfigEntryString extends ConfigEntry {
-        private final String value;
-        private final String description;
-
-        public ConfigEntryString(String name, String description) {
-            super(name, description);
-            this.value = name;
-            this.description = description;
-        }
-
-        //This was ConfigType.INT before changed it to STRING
-        @Override
-        public ConfigType getType() {
-            return ConfigType.STRING;
-        }
-
-    }
-
-    public static class ConfigEntryInt extends ConfigEntry {
-        private final int value;
-        private final String description;
-
-        public ConfigEntryInt(int name, String description) {
-            super(name, description);
-            this.value = name;
-            this.description = description;
-        }
-
-        @Override
-        public ConfigType getType() {
-            return ConfigType.INT;
-        }
-
-    }
-    public static class ConfigEntryBool extends ConfigEntry {
-        private final boolean value;
-        private final String description;
-
-        public ConfigEntryBool(boolean name, String description) {
-            super(name, description);
-            this.value = name;
-            this.description = description;
-        }
-
-        @Override
-        public ConfigType getType() {
-            return ConfigType.BOOLEAN;
-        }
-
-    }
-
-    public enum ConfigType implements StringRepresentable {
-        STRING,
-        BOOLEAN,
-        INT;
-
-        @Override
-        public String getSerializedName() {
-            return name().toLowerCase();
-        }
-    }
-
-
 }
--- common/src/main/java/com/st0x0ef/stellaris/common/config/CustomConfig.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/config/CustomConfig.java
@@ -1,6 +1,5 @@
 package com.st0x0ef.stellaris.common.config;
 
-import com.google.common.reflect.TypeToken;
 import com.google.gson.JsonObject;
 import com.st0x0ef.stellaris.Stellaris;
 import dev.architectury.platform.Platform;
@@ -10,48 +9,42 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class CustomConfig {
-    public static Map<String, ConfigEntry> CONFIG = new HashMap<>();
+    public static Map<String, ConfigEntry<?>> CONFIG = new HashMap<>();
 
     public static void init() {
         loadConfigFile();
         addEntries();
-        writeConfigFile(CONFIG, "stellaris.json");
-        //config = Stellaris.GSON.fromJson(Platform.getStreamForResource("stellaris.json"), Map.class);
+        writeConfigFile("stellaris.json");
 
     }
 
     public static void addEntries() {
 
-        addEntry("test", new ConfigEntry.ConfigEntryString("1", "uwu"));
-        addEntry("test2", new ConfigEntry.ConfigEntryInt(1, "uwu"));
-        addEntry("test3", new ConfigEntry.ConfigEntryBool(true, "uwu"));
+        addEntry("test", new ConfigEntry<Boolean>( true,"uwu"));
+        addEntry("test2", new ConfigEntry<String>("test","uwu"));
+        addEntry("test3", new ConfigEntry<String>("test","uwu"));
+
     }
 
-    public static void addEntry(String name, ConfigEntry entry) {
-        Stellaris.LOG.error(name);
+    public static void addEntry(String name, ConfigEntry<?> entry) {
 
         if(CONFIG.get(name) == null) {
-
             CONFIG.put(name, entry);
         } else {
 
-            Stellaris.LOG.error(CONFIG.get(name).getName());
-            Stellaris.LOG.error(entry.getName());
-
-            CONFIG.put(name, new ConfigEntry.ConfigEntryString(CONFIG.get(name).getName(), CONFIG.get(name).getDescription()));
+            CONFIG.put(name, new ConfigEntry(CONFIG.get(name).getValue(), entry.getDescription()));
         }
 
 
     }
 
-    public static void writeConfigFile(Map<?, ?> config, String path) {
+    public static void writeConfigFile( String path) {
         try {
             FileWriter file = new FileWriter(Platform.getConfigFolder() + "/" + path);
-            file.write(Stellaris.GSON.toJson(config));
+            file.write(Stellaris.GSON.toJson(CONFIG));
             file.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -61,21 +54,16 @@ public class CustomConfig {
     public static void loadConfigFile() {
 
         String jsonString;
-        HashMap<String, ConfigEntry> map = new HashMap<>();
 
         try {
             jsonString = readFileAsString(Platform.getConfigFolder() + "/stellaris.json");
             JsonObject jsonObject = Stellaris.GSON.fromJson(jsonString, JsonObject.class);
 
             jsonObject.getAsJsonObject().entrySet().forEach(entry -> {
-                String key = entry.getKey();
-                JsonObject value = entry.getValue().getAsJsonObject();
-                if(jsonObject == null) {
-                    return;
-                }
-                CONFIG.put(key, new ConfigEntry.ConfigEntryString(value.get("value").toString(), value.get("description").getAsString()));
-
+                ConfigEntry<?> obj2 = Stellaris.GSON.fromJson(entry.getValue(), ConfigEntry.class);
+                CONFIG.put(entry.getKey(), obj2);
             });
+
         } catch (Exception e) {
             e.printStackTrace();
         }
