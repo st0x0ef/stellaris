df29efd0bdd9069724b82074fb0454f0c6b4fbe4
TATHAN <costa.tathan@gmail.com>
2024-03-25 22:21:28 +0100

    Add more types

--- common/src/main/java/com/st0x0ef/stellaris/client/screens/ConfigScreen.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/screens/ConfigScreen.java
@@ -1,17 +1,18 @@
 package com.st0x0ef.stellaris.client.screens;
 
+import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.common.config.ConfigEntry;
 import com.st0x0ef.stellaris.common.config.CustomConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.StringWidget;
-import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.components.*;
 import net.minecraft.client.gui.layouts.FrameLayout;
 import net.minecraft.client.gui.layouts.GridLayout;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
+import net.minecraft.client.gui.screens.worldselection.WorldSelectionList;
 import net.minecraft.network.chat.Component;
 
 import java.util.Map;
@@ -35,17 +36,17 @@ public class ConfigScreen extends Screen {
         GridLayout.RowHelper rowHelper = gridLayout.createRowHelper(2);
 
         config.forEach( (string, configEntry) -> {
-            Button button1 = Button.builder(Component.literal(configEntry.getValue().toString()), button -> {System.out.println("You clicked button1!");})
-                    .size(50,25)
-                    .tooltip(new Tooltip(Component.literal(configEntry.getDescription()),null))
-                    .build();
+            if(configEntry.getType() == Boolean.class) {
+                Stellaris.LOG.error("Boolean");
+            }
+
             StringWidget widget = new StringWidget(Component.literal(string), this.font);
+
+
             rowHelper.addChild(widget);
-            rowHelper.addChild(button1);
+            addTypeWidgets(configEntry, rowHelper);
 
         });
-
-        //addWidget(button1);
 
         gridLayout.arrangeElements();
         FrameLayout.alignInRectangle(gridLayout, -120, this.height / 6 , this.width, this.height, 0.5F, 0.0F);
@@ -75,4 +76,20 @@ public class ConfigScreen extends Screen {
         super.render(guiGraphics, i, j, f);
         guiGraphics.drawCenteredString(this.font, this.title, this.width / 2, 20, 16777215);
     }
+
+    public void addTypeWidgets(ConfigEntry entry, GridLayout.RowHelper rowHelper) {
+        if(entry.getType() == Boolean.class) {
+            Checkbox checkbox = Checkbox.builder(Component.literal(entry.getValue().toString()), this.font)
+                    .tooltip(new Tooltip(Component.literal(entry.getDescription()),null))
+                    .build();
+            rowHelper.addChild(checkbox);
+        } else if (entry.getType() == String.class || entry.getType() == Integer.class || entry.getType() == Float.class || entry.getType() == Double.class) {
+            EditBox button = new EditBox(this.font, 50, 30, Component.literal(entry.getValue().toString()));
+            rowHelper.addChild(button);
+        } else {
+            StringWidget widget = new StringWidget(Component.literal("This config type is not supported. Use the manual config"), this.font);
+            rowHelper.addChild(widget);
+        }
+    }
+
 }
