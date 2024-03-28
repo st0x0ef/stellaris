3b8b4db63ec4d24af82f5155fc9928ee234350c0
lukeon <163727888+lukeon214@users.noreply.github.com>
2024-03-28 22:03:18 +0100

    Glacio gone :(

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/FoodRegistry.java
+++
@@ -1,9 +0,0 @@
-package com.st0x0ef.stellaris.common.registry;
-
-import net.minecraft.world.food.FoodProperties;
-
-public class FoodRegistry {
-    //**FOOD REGiSTRY**//
-    public static final FoodProperties CHEESE = new FoodProperties.Builder().nutrition(3).saturationMod(1).build();
-
-}
