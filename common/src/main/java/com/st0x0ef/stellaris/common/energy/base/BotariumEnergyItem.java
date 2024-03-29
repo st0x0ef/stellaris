8cf9c45c3b3a4946db3683efcce0840aa9b52c29
Fej1Dev <guymoshegg007@gmail.com>
2024-03-29 19:12:24 +0300

    (Botarium)Energy part 1

---
+++ common/src/main/java/com/st0x0ef/stellaris/common/energy/base/BotariumEnergyItem.java
@@ -0,0 +1,20 @@
+package com.st0x0ef.stellaris.common.energy.base;
+
+import com.st0x0ef.stellaris.common.energy.util.Updatable;
+import net.minecraft.world.item.ItemStack;
+
+/**
+ * Functional interface that represents an item that can provide an energy storage container.
+ *
+ * @param <T> The type of energy storage container. Botarium provides a default implementation for this with {@link WrappedItemEnergyContainer}.
+ */
+@FunctionalInterface
+public interface BotariumEnergyItem<T extends EnergyContainer & Updatable> {
+    /**
+     * Retrieves the energy storage container for the given holder.
+     *
+     * @param holder The holder item stack.
+     * @return The energy storage container.
+     */
+    T getEnergyStorage(ItemStack holder);
+}
