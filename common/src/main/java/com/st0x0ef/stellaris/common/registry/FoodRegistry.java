579ce57fe22016d853796075057d5d25189241d2
eirmax <eirmax8@gmail.com>
2024-03-30 17:36:44 +0700

    Cosmo Food Beta

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/FoodRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/FoodRegistry.java
@@ -5,5 +5,9 @@ import net.minecraft.world.food.FoodProperties;
 public class FoodRegistry {
     //**FOOD REGiSTRY**//
     public static final FoodProperties CHEESE = new FoodProperties.Builder().nutrition(3).saturationMod(1).build();
+    public static final FoodProperties CANNED_STEAK = (new FoodProperties.Builder()).nutrition(8).saturationMod(0.8F).meat().build();
+    public static final FoodProperties BERRY_JUICE = (new FoodProperties.Builder()).nutrition(3).saturationMod(0.4F).meat().build();
+    public static final FoodProperties COSMO_BREAD = (new FoodProperties.Builder()).nutrition(6).saturationMod(0.6F).meat().build();
+
 
 }
