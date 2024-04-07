f2425f68ffdadbb15ae8c69ea10d3199f93f41f4
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2024-04-07 13:25:09 -0400

    add radioactivity level

--- common/src/main/java/com/st0x0ef/stellaris/common/effects/RadioactiveEffect.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/effects/RadioactiveEffect.java
@@ -2,19 +2,31 @@ package com.st0x0ef.stellaris.common.effects;
 
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
 public class RadioactiveEffect extends MobEffect {
+    public int level;
 
-    public RadioactiveEffect(MobEffectCategory mobEffectCategory, int i) {
-        super(mobEffectCategory, i);
+    public RadioactiveEffect(MobEffectCategory mobEffectCategory, int color, int level) {
+        super(mobEffectCategory, color);
+        this.level = level;
     }
 
-    public void applyEffectTick(LivingEntity livingEntity, int i) {
-        super.applyEffectTick(livingEntity, i);
+    public void applyEffectTick(LivingEntity livingEntity, int color) {
+        super.applyEffectTick(livingEntity, color);
         if (livingEntity.getHealth() > 0.0F) {
-            livingEntity.hurt(livingEntity.damageSources().magic(), 2.0F);
-        }
+            if (this.level == 1 || this.level == 2) {
+                livingEntity.hurt(livingEntity.damageSources().magic(), 0.5f);
+            }
+            if (this.level == 2 || this.level == 3) {
+                livingEntity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 20));
 
+                if (this.level == 3) {
+                    livingEntity.hurt(livingEntity.damageSources().magic(), 1.0f);
+                }
+            }
+        }
     }
 
     public boolean shouldApplyEffectTickThisTick(int i, int j) {
--- common/src/main/java/com/st0x0ef/stellaris/common/registry/EffectsRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/EffectsRegistry.java
@@ -14,7 +14,9 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 public class EffectsRegistry {
     public static final DeferredRegister<MobEffect> MOB_EFFECT = DeferredRegister.create(Stellaris.MODID, Registries.MOB_EFFECT);
 
-    public static final RegistrySupplier<MobEffect> RADIOACTIVE = MOB_EFFECT.register("radioactive", () -> new RadioactiveEffect(MobEffectCategory.HARMFUL, 8889187));
+    public static final RegistrySupplier<MobEffect> RADIOACTIVE_LVL_1 = MOB_EFFECT.register("radioactive_1", () -> new RadioactiveEffect(MobEffectCategory.HARMFUL, 8889187, 1));
+    public static final RegistrySupplier<MobEffect> RADIOACTIVE_LVL_2 = MOB_EFFECT.register("radioactive_2", () -> new RadioactiveEffect(MobEffectCategory.HARMFUL, 8889187, 2));
+    public static final RegistrySupplier<MobEffect> RADIOACTIVE_LVL_3 = MOB_EFFECT.register("radioactive_3", () -> new RadioactiveEffect(MobEffectCategory.HARMFUL, 8889187, 3));
 }
 
 
