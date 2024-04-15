a64b2fbb5fc603728f65888d862b8a9d5a4178c2
TATHAN <costa.tathan@gmail.com>
2024-04-15 18:00:38 +0200

    Fix Solar Panel

--- common/src/main/java/com/st0x0ef/stellaris/common/blocks/machines/SolarPanelBlock.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/blocks/machines/SolarPanelBlock.java
@@ -4,12 +4,21 @@ import com.mojang.serialization.MapCodec;
 import com.st0x0ef.stellaris.common.blocks.entities.machines.GeneratorBlockEntityTemplate;
 import com.st0x0ef.stellaris.common.blocks.entities.machines.SolarPanelEntity;
 import com.st0x0ef.stellaris.common.energy.EnergyApi;
+import com.st0x0ef.stellaris.common.menus.SolarPanelMenu;
 import com.st0x0ef.stellaris.common.registry.EntityRegistry;
+import dev.architectury.registry.menu.ExtendedMenuProvider;
+import dev.architectury.registry.menu.MenuRegistry;
+import io.netty.buffer.Unpooled;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
@@ -17,6 +26,7 @@ import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SolarPanelBlock extends GeneratorBlockTemplate {
@@ -44,9 +54,31 @@ public class SolarPanelBlock extends GeneratorBlockTemplate {
         if (!level.isClientSide()) {
             BlockEntity blockEntity = level.getBlockEntity(blockPos);
             if (blockEntity instanceof SolarPanelEntity) {
-                player.openMenu(blockState.getMenuProvider(level, blockPos));
+                MenuRegistry.openExtendedMenu((ServerPlayer) player, this.getMenuProvider(blockState, level, blockPos))  ;
+                }
             }
-        }
         return InteractionResult.SUCCESS;
     }
+
+    @Nullable
+    @Override
+    public ExtendedMenuProvider getMenuProvider(BlockState blockState, Level level, BlockPos blockPos) {
+        return new ExtendedMenuProvider() {
+            @Override
+            public void saveExtraData(FriendlyByteBuf packetByteBuf) {
+                packetByteBuf.writeBlockPos(blockPos);
+            }
+
+            @Override
+            public Component getDisplayName() {
+                return Component.literal("Recipe Creator");
+            }
+
+            @Override
+            public @NotNull AbstractContainerMenu createMenu(int syncId, Inventory inv, Player player) {
+                return SolarPanelMenu.create(syncId, inv, new FriendlyByteBuf(Unpooled.buffer()).writeBlockPos(blockPos));
+            }
+        };
+    }
+
 }
