c7208c585506ce0f093b7bb457f1c019d03ebc64
Fej1Dev <guymoshegg007@gmail.com>
2024-04-16 16:02:38 +0300

    Correct cable shape

--- common/src/main/java/com/st0x0ef/stellaris/common/blocks/machines/Cable.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/blocks/machines/Cable.java
@@ -16,12 +16,17 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.Property;
 import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
 public class Cable extends BaseEnergyBlock {
+    private static final Direction[] DIRECTIONS = Direction.values();
     public static final BooleanProperty NORTH = PipeBlock.NORTH;
     public static final BooleanProperty EAST = PipeBlock.EAST;
     public static final BooleanProperty SOUTH = PipeBlock.SOUTH;
@@ -29,6 +34,7 @@ public class Cable extends BaseEnergyBlock {
     public static final BooleanProperty UP = PipeBlock.UP;
     public static final BooleanProperty DOWN = PipeBlock.DOWN;
     private static final Map<Direction, BooleanProperty> PROPERTY_BY_DIRECTION = PipeBlock.PROPERTY_BY_DIRECTION;
+    protected final VoxelShape[] shapeByIndex;
 
     public Cable(Properties properties) {
         super(properties);
@@ -39,6 +45,7 @@ public class Cable extends BaseEnergyBlock {
                 .setValue(WEST, false))
                 .setValue(UP, false))
                 .setValue(DOWN, false));
+        this.shapeByIndex = this.makeShapes(0.125f);
     }
 
     @Nullable
@@ -111,5 +118,49 @@ public class Cable extends BaseEnergyBlock {
     @Override
     public RenderShape getRenderShape(BlockState blockState) {
         return RenderShape.MODEL;
+    }
+
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return this.shapeByIndex[this.getAABBIndex(state)];
+    }
+
+    private VoxelShape[] makeShapes(float apothem) {
+        float f = 0.5F - apothem;
+        float g = 0.5F + apothem;
+        VoxelShape voxelShape = Block.box((double)(f * 16.0F), (double)(f * 16.0F), (double)(f * 16.0F), (double)(g * 16.0F), (double)(g * 16.0F), (double)(g * 16.0F));
+        VoxelShape[] voxelShapes = new VoxelShape[DIRECTIONS.length];
+
+        for(int i = 0; i < DIRECTIONS.length; ++i) {
+            Direction direction = DIRECTIONS[i];
+            voxelShapes[i] = Shapes.box(0.5 + Math.min((double)(-apothem), (double)direction.getStepX() * 0.5), 0.5 + Math.min((double)(-apothem), (double)direction.getStepY() * 0.5), 0.5 + Math.min((double)(-apothem), (double)direction.getStepZ() * 0.5), 0.5 + Math.max((double)apothem, (double)direction.getStepX() * 0.5), 0.5 + Math.max((double)apothem, (double)direction.getStepY() * 0.5), 0.5 + Math.max((double)apothem, (double)direction.getStepZ() * 0.5));
+        }
+
+        VoxelShape[] voxelShapes2 = new VoxelShape[64];
+
+        for(int j = 0; j < 64; ++j) {
+            VoxelShape voxelShape2 = voxelShape;
+
+            for(int k = 0; k < DIRECTIONS.length; ++k) {
+                if ((j & 1 << k) != 0) {
+                    voxelShape2 = Shapes.or(voxelShape2, voxelShapes[k]);
+                }
+            }
+
+            voxelShapes2[j] = voxelShape2;
+        }
+
+        return voxelShapes2;
+    }
+
+    protected int getAABBIndex(BlockState state) {
+        int i = 0;
+
+        for(int j = 0; j < DIRECTIONS.length; ++j) {
+            if ((Boolean)state.getValue((Property)PROPERTY_BY_DIRECTION.get(DIRECTIONS[j]))) {
+                i |= 1 << j;
+            }
+        }
+
+        return i;
     }
 }
