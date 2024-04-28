6da9525e888f5fd7ed50dcafb6ce61aa0a8b90d7
gabrielgaudreault <gabriel.gaudreault3@gmail.com>
2024-04-28 12:04:45 -0400

    small fix

--- common/src/main/java/com/st0x0ef/stellaris/mixin/EntityEatMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/EntityEatMixin.java
@@ -49,17 +49,19 @@ public abstract class EntityEatMixin extends LivingEntity {
                 food.shrink(1);
             }
 
-            if (food.is(Items.GLASS_BOTTLE)) {
-                if (food.getComponents().get(DataComponents.POTION_CONTENTS).is(Potions.WATER)) {
-                    if (entity instanceof Player player) {
-                        if (player.getItemInHand(InteractionHand.OFF_HAND).getItem() instanceof CanItem can) {
-                            player.getFoodData().eat(can.getFoodProperties().nutrition(), can.getFoodProperties().saturation());
-                        }
+
+
+            this.gameEvent(GameEvent.EAT);
+        }
+
+        if (food.is(Items.GLASS_BOTTLE)) {
+            if (food.getComponents().get(DataComponents.POTION_CONTENTS).is(Potions.WATER)) {
+                if (entity instanceof Player player) {
+                    if (player.getItemInHand(InteractionHand.OFF_HAND).getItem() instanceof CanItem can) {
+                        player.getFoodData().eat(can.getFoodProperties().nutrition(), can.getFoodProperties().saturation());
                     }
                 }
             }
-
-            this.gameEvent(GameEvent.EAT);
         }
 
         cir.setReturnValue(food);
