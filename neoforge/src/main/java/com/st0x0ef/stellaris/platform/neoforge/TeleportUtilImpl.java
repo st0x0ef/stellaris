1abefbdb61519a3595b0cf842b0ad348df67e30d
TATHAN <costa.tathan@gmail.com>
2024-05-25 12:19:45 +0200

    Add Planet Teleportation

---
+++ neoforge/src/main/java/com/st0x0ef/stellaris/platform/neoforge/TeleportUtilImpl.java
@@ -0,0 +1,39 @@
+package com.st0x0ef.stellaris.platform.neoforge;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.portal.PortalInfo;
+import net.minecraft.world.phys.Vec3;
+import net.neoforged.neoforge.common.util.ITeleporter;
+
+import java.util.function.Function;
+
+public class TeleportUtilImpl {
+
+    public static void teleportToPlanet(Entity entity, ServerLevel level, int yPos) {
+        Vec3 newPos = new Vec3(entity.getX(), yPos, entity.getZ());
+        PortalInfo portalInfo = new PortalInfo(newPos, Vec3.ZERO, entity.yRotO, entity.xRotO);
+
+        entity.changeDimension(level, new ITeleporter() {
+            @Override
+            public PortalInfo getPortalInfo(Entity entity, ServerLevel destWorld, Function<ServerLevel, PortalInfo> defaultPortalInfo) {
+                Vec3 pos = new Vec3(entity.position().x, yPos, entity.position().z);
+
+                return new PortalInfo(pos, Vec3.ZERO, entity.getYRot(), entity.getXRot());
+            }
+
+            @Override
+            public boolean isVanilla() {
+                return false;
+            }
+
+            @Override
+            public boolean playTeleportSound(ServerPlayer player, ServerLevel sourceWorld, ServerLevel destWorld) {
+                return false;
+            }
+        });
+    }
+
+
+}
