b4dce8d6f19eb587087793437cf5947d837d19a3
TATHAN <costa.tathan@gmail.com>
2024-05-26 13:55:37 +0200

    add translatable field for planets

--- common/src/main/java/com/st0x0ef/stellaris/common/data/planets/Planet.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/data/planets/Planet.java
@@ -4,11 +4,13 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 
 public record Planet(
         String system,
+        String translatable,
         ResourceKey<Level> dimension,
         ResourceKey<Level> orbit,
         boolean oxygen, int temperature, int distanceFromEarth, float gravity,
@@ -17,6 +19,7 @@ public record Planet(
 ) {
     public static final Codec<Planet> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.STRING.fieldOf("system").forGetter(Planet::system),
+            Codec.STRING.fieldOf("translatable").forGetter(Planet::translatable),
             ResourceKey.codec(Registries.DIMENSION).fieldOf("level").forGetter(Planet::dimension),
             ResourceKey.codec(Registries.DIMENSION).fieldOf("orbit").forGetter(Planet::orbit),
             Codec.BOOL.fieldOf("oxygen").forGetter(Planet::oxygen),
@@ -26,14 +29,16 @@ public record Planet(
             PlanetTextures.CODEC.fieldOf("textures").forGetter(Planet::textures)
 
 
+
     ).apply(instance, Planet::new));
 
     public static Planet fromNetwork(FriendlyByteBuf buffer) {
-        return new Planet(buffer.readUtf(), buffer.readResourceKey(Registries.DIMENSION), buffer.readResourceKey(Registries.DIMENSION), buffer.readBoolean(), buffer.readInt(), buffer.readInt(), buffer.readFloat(), PlanetTextures.fromNetwork(buffer));
+        return new Planet(buffer.readUtf(), buffer.readUtf(), buffer.readResourceKey(Registries.DIMENSION), buffer.readResourceKey(Registries.DIMENSION), buffer.readBoolean(), buffer.readInt(), buffer.readInt(), buffer.readFloat(), PlanetTextures.fromNetwork(buffer));
     }
 
     public  void toNetwork(FriendlyByteBuf buffer) {
         buffer.writeUtf(this.system);
+        buffer.writeUtf(this.translatable);
         buffer.writeResourceKey(this.dimension);
         buffer.writeResourceKey(this.orbit);
         buffer.writeBoolean(this.oxygen);
@@ -41,5 +46,9 @@ public record Planet(
         buffer.writeInt(this.distanceFromEarth);
         buffer.writeFloat(this.gravity);
         this.textures.toNetwork(buffer);
+    }
+
+    public Component getTranslation() {
+        return Component.translatable(this.translatable);
     }
 }
