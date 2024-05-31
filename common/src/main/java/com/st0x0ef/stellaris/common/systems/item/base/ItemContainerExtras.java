4cec60676660c56e407784256c3e4c253808043d
Fej1Dev <guymoshegg007@gmail.com>
2024-05-31 12:53:52 +0300

    lookup api?

---
+++ common/src/main/java/com/st0x0ef/stellaris/common/systems/item/base/ItemContainerExtras.java
@@ -0,0 +1,7 @@
+package com.st0x0ef.stellaris.common.systems.item.base;
+
+import net.minecraft.world.item.ItemStack;
+
+public interface ItemContainerExtras {
+    void setStackInSlot(int slot, ItemStack stack);
+}
