3c554ea45a0648b168f44f55d130fa0ceb4be428
miyaiho <mimiccube663@gmail.com>
2024-06-01 12:27:16 +0900

    Add MilkyWayScreen Command

--- common/src/main/java/com/st0x0ef/stellaris/common/commands/StellarisCommands.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/commands/StellarisCommands.java
@@ -1,6 +1,7 @@
 package com.st0x0ef.stellaris.common.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
 import com.st0x0ef.stellaris.common.utils.PlanetUtil;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
@@ -12,7 +13,15 @@ public class StellarisCommands {
         dispatcher.register(Commands.literal("stellaris")
                 .then(Commands.literal("planetScreen")
                         .requires(c -> c.hasPermission(2))
-                        .executes((Command) -> PlanetUtil.openPlanetSelectionMenu(Command.getSource().getPlayer())))
-        );
+                        .executes((CommandContext<CommandSourceStack> context) -> {
+                            PlanetUtil.openPlanetSelectionMenu(context.getSource().getPlayer());
+                            return 0;
+                        }))
+                .then(Commands.literal("galaxyScreen")
+                        .requires(c -> c.hasPermission(2))
+                        .executes((CommandContext<CommandSourceStack> context) -> {
+                            PlanetUtil.openMilkyWayMenu(context.getSource().getPlayer());
+                            return 0;
+                        })));
     }
 }
