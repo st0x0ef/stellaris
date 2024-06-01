d54ab515b0adcffb9451d680ea770eb91582de2c
miyaiho <mimiccube663@gmail.com>
2024-06-01 19:45:57 +0900

    Add Translatable Registry

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/TranslatableRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/TranslatableRegistry.java
@@ -1,2 +1,7 @@
-package com.st0x0ef.stellaris.common.registry;public class TranslatableRegistry {
+package com.st0x0ef.stellaris.common.registry;
+
+import net.minecraft.network.chat.Component;
+
+public class TranslatableRegistry {
+    public static final Component holdShift = Component.translatable("translatable.stellaris.registry.holdshift");
 }
