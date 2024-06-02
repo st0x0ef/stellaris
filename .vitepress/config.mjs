c25c1bd6a2864b2da6b032f888a2125969ab709c
TATHAN <costa.tathan@gmail.com>
2024-06-02 15:14:27 +0200

    fix base

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: '/stellaris/',
   title: "Stellaris Project Documentation",
   description: "Learn how to create your own planets !",
   themeConfig: {
