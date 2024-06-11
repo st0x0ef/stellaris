b8d63e249bbadbb7de7acbe294fc549351e01160
TATHAN <costa.tathan@gmail.com>
2024-06-11 15:40:01 +0200

    add underground oil lakes

---
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/BiomeModificationsRegistry.java
@@ -0,0 +1,20 @@
+package com.st0x0ef.stellaris.common.registry;
+
+import com.st0x0ef.stellaris.Stellaris;
+import dev.architectury.registry.level.biome.BiomeModifications;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+
+public class BiomeModificationsRegistry {
+
+    public static void register() {
+        ResourceKey<PlacedFeature> UNDERGROUND_OIL_LAKE = ResourceKey.create(Registries.PLACED_FEATURE, new ResourceLocation(Stellaris.MODID,"underground_oil_lake"));
+
+        BiomeModifications.addProperties((context) -> context.hasTag(BiomeTags.IS_OVERWORLD), ((biomeContext, mutable) -> mutable.getGenerationProperties().addFeature(GenerationStep.Decoration.LAKES, UNDERGROUND_OIL_LAKE)));
+    }
+}
