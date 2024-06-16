001cf87de0faea0786f8984ee5576fec17b20a87
st0x0ef <75510490+st0x0ef@users.noreply.github.com>
2024-06-15 20:59:40 -0400

    trying to fix config crash when getting value

--- common/src/main/java/com/st0x0ef/stellaris/common/effects/RadioactiveEffect.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/effects/RadioactiveEffect.java
@@ -8,6 +8,7 @@ import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
 public class RadioactiveEffect extends MobEffect {
 
@@ -19,7 +20,7 @@ public class RadioactiveEffect extends MobEffect {
     public boolean applyEffectTick(LivingEntity livingEntity, int level) {
         if (livingEntity.getHealth() > 0.0F) {
             if (level == 0) {
-                livingEntity.hurt(DamageSourceRegistry.of(livingEntity.level(), DamageSourceRegistry.RADIATIONS), 0.7f);
+                livingEntity.hurt(DamageSourceRegistry.of(livingEntity.level(), DamageSourceRegistry.RADIATIONS), 0.5f);
             } else if (level == 1) {
                 livingEntity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 80));
                 livingEntity.hurt(DamageSourceRegistry.of(livingEntity.level(), DamageSourceRegistry.RADIATIONS), 0.5f);
@@ -28,31 +29,18 @@ public class RadioactiveEffect extends MobEffect {
                 livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 80));
                 livingEntity.hurt(DamageSourceRegistry.of(livingEntity.level(), DamageSourceRegistry.RADIATIONS), 1f);
             }
-
-            return true;
         }
 
-
-        return false;
+        return true;
     }
 
     public boolean shouldApplyEffectTickThisTick(int i, int j) {
         int k = 25 >> j;
-        if (k > 0) {
-            return i % k == 0;
-        } else {
-            return true;
-        }
+        return k == 0 || i % k == 0;
     }
 
-    public void onEffectStarted(LivingEntity livingEntity, int i) {
-        if (i == 1 || i == 2) {
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 80));
-            livingEntity.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 80));
-        }
-    }
-
-    public void onEffectAdded(LivingEntity livingEntity, int amplifier){
-        livingEntity.level().playSound(null, livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), soundOnAdded, livingEntity.getSoundSource(), 3.0F, 1.0F);
+    public void onEffectAdded(LivingEntity livingEntity, int level) {
+        super.onEffectAdded(livingEntity, level);
+        livingEntity.level().playSound((Player) livingEntity, livingEntity.getX(), livingEntity.getY(), livingEntity.getZ(), soundOnAdded, livingEntity.getSoundSource(), 3.0F, 1.0F);
     }
 }
--- common/src/main/java/com/st0x0ef/stellaris/common/oxygen/OxygenUtils.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/oxygen/OxygenUtils.java
@@ -3,5 +3,5 @@ package com.st0x0ef.stellaris.common.oxygen;
 import com.st0x0ef.stellaris.common.config.CustomConfig;
 
 public class OxygenUtils {
-    public static final int maxOxygenByBlock = (int)CustomConfig.getValue("maxOxygenPerBlock");
+    public static final Integer maxOxygenByBlock = (Integer) CustomConfig.getValue("maxOxygenPerBlock");
 }
