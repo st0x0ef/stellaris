25f4520c2f8e4cf400929d4a278a82f55ac07b35
VoxelShape <mimiccube664@gmail.com>
2024-06-16 23:27:19 +0900

    Fix SkyRendererNeoforge

--- neoforge/src/main/java/com/st0x0ef/stellaris/neoforge/client/renderer/SkyRendererNeoForge.java
+++ neoforge/src/main/java/com/st0x0ef/stellaris/neoforge/client/renderer/SkyRendererNeoForge.java
@@ -2,18 +2,13 @@ package com.st0x0ef.stellaris.neoforge.client.renderer;
 
 import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.client.skys.renderer.SkyRenderer;
-import com.st0x0ef.stellaris.common.utils.PlanetUtil;
-import net.minecraft.client.CloudStatus;
-import net.minecraft.client.GraphicsStatus;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.client.event.RenderLevelStageEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 
 import java.util.Objects;
 
@@ -27,6 +22,6 @@ public class SkyRendererNeoForge {
         Player p = mc.player;
         ResourceKey<Level> dim = Objects.requireNonNull(p).level().dimension();
 
-        SkyRenderer.render(dim, e.getPartialTick(), e.getModelViewMatrix(), e.getPoseStack(), e.getProjectionMatrix());
+        SkyRenderer.render(dim, e.getPartialTick(), e.getProjectionMatrix(), e.getPoseStack(), e.getModelViewMatrix());
     }
 }
