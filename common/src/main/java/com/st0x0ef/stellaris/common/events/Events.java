390bb0f474c8f7e3d9bd40dd030ebb32cfb3eee5
st0x0ef <75510490+st0x0ef@users.noreply.github.com>
2024-06-19 10:42:22 -0400

    effect...

--- common/src/main/java/com/st0x0ef/stellaris/common/events/Events.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/events/Events.java
@@ -2,51 +2,39 @@ package com.st0x0ef.stellaris.common.events;
 
 import com.st0x0ef.stellaris.client.skys.renderer.SkyRenderer;
 import com.st0x0ef.stellaris.client.skys.type.RenderableType;
-import com.st0x0ef.stellaris.common.config.CustomConfig;
 import com.st0x0ef.stellaris.common.items.RadiationItem;
-import com.st0x0ef.stellaris.common.items.RadioactiveBlockItem;
-import com.st0x0ef.stellaris.common.items.RadioactiveItem;
 import com.st0x0ef.stellaris.common.registry.EffectsRegistry;
 import com.st0x0ef.stellaris.common.utils.PlanetUtil;
-import dev.architectury.event.events.common.PlayerEvent;
 import dev.architectury.event.events.common.TickEvent;
-import net.minecraft.core.NonNullList;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.item.ItemStack;
 
 import java.util.Objects;
 
 public class Events {
-    private static int tickSinceLastRadioactiveCheck;
+    private static int tickSinceLastRadioactiveCheck = 100;
     public static boolean isCustomClouds;
 
     public static void registerEvents() {
-
-       PlayerEvent.OPEN_MENU.register((Player player, AbstractContainerMenu menu) -> {
-          NonNullList<ItemStack> items =  menu.getItems();
-          items.forEach(item -> {
-              if(item.getItem() instanceof RadioactiveItem radioactiveItem) {
-                    player.addEffect(new MobEffectInstance(EffectsRegistry.RADIOACTIVE, 100, radioactiveItem.getRadiationLevel()));
-              } else if (item.getItem() instanceof RadioactiveBlockItem radioactiveBlockItem){
-                  player.addEffect(new MobEffectInstance(EffectsRegistry.RADIOACTIVE, 100, radioactiveBlockItem.getRadiationLevel()));
-              }
-          });
-       });
-
        TickEvent.PLAYER_POST.register(player -> {
-           if(tickSinceLastRadioactiveCheck == 100) {
+           if (tickSinceLastRadioactiveCheck == 0) {
                player.getInventory().items.forEach(itemStack -> {
-                   if(itemStack.getItem() instanceof RadiationItem radioactiveItem) {
+                   if (itemStack.getItem() instanceof RadiationItem radioactiveItem) {
                        player.addEffect(new MobEffectInstance(EffectsRegistry.RADIOACTIVE, 100, radioactiveItem.getRadiationLevel()));
                    }
                });
-               tickSinceLastRadioactiveCheck = 0;
+
+               if (player.hasContainerOpen()) {
+                   player.containerMenu.getItems().forEach(itemStack -> {
+                       if (itemStack.getItem() instanceof RadiationItem radioactiveItem) {
+                           player.addEffect(new MobEffectInstance(EffectsRegistry.RADIOACTIVE, 100, radioactiveItem.getRadiationLevel()));
+                       }
+                   });
+               }
+               tickSinceLastRadioactiveCheck = 100;
            }
 
-           tickSinceLastRadioactiveCheck++;
+           tickSinceLastRadioactiveCheck--;
 
            RenderableType renderableType = SkyRenderer.getRenderableType(player.level().dimension());
 
--- common/src/main/java/com/st0x0ef/stellaris/common/registry/EffectsRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/EffectsRegistry.java
@@ -3,7 +3,7 @@ package com.st0x0ef.stellaris.common.registry;
 import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.common.effects.RadioactiveEffect;
 import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.RegistrySupplier;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
@@ -11,5 +11,5 @@ import net.minecraft.world.effect.MobEffectCategory;
 public class EffectsRegistry {
     public static final DeferredRegister<MobEffect> MOB_EFFECTS = DeferredRegister.create(Stellaris.MODID, Registries.MOB_EFFECT);
 
-    public static final RegistrySupplier<MobEffect> RADIOACTIVE = MOB_EFFECTS.register("radioactive", () -> new RadioactiveEffect(MobEffectCategory.HARMFUL, 8889187));
+    public static final Holder<MobEffect> RADIOACTIVE = MOB_EFFECTS.register("radioactive", () -> new RadioactiveEffect(MobEffectCategory.HARMFUL, 8889187));
 }
