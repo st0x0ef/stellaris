8248938509d68bb5990fccf80ae3293672e8a822
st0x0ef <75510490+st0x0ef@users.noreply.github.com>
2024-06-22 16:31:39 -0400

    improve code

--- common/src/main/java/com/st0x0ef/stellaris/mixin/EntityTick.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/EntityTick.java
@@ -14,16 +14,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(LivingEntity.class)
 public abstract class EntityTick {
     @Unique
-    private long stellaris$lastOxygenCheck;
+    private long stellaris$tickSinceLastOxygenCheck;
 
     @Inject(at = @At(value = "HEAD"), method = "tick")
     private void tick(CallbackInfo info) {
         LivingEntity entity = (LivingEntity) ((Object) this);
 
-        long now = System.currentTimeMillis();
-        if((now - stellaris$lastOxygenCheck) > 1000){
+        if(stellaris$tickSinceLastOxygenCheck > 20){
             EntityOxygen.tick(entity);
-            stellaris$lastOxygenCheck = now;
+            stellaris$tickSinceLastOxygenCheck = 0;
         }
+
+        stellaris$tickSinceLastOxygenCheck++;
     }
 }
