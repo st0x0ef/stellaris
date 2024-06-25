1119a611bb7d170b58b168da1d446fbeacc7b1e9
Fej1Dev <guymoshegg007@gmail.com>
2024-06-26 01:49:07 +0300

    neater energy registering

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/LookupApiRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/LookupApiRegistry.java
@@ -1,86 +1,34 @@
 package com.st0x0ef.stellaris.common.registry;
 
 import com.st0x0ef.stellaris.common.blocks.entities.machines.*;
-import com.st0x0ef.stellaris.common.blocks.entities.machines.oxygen.OxygenDistributorBlockEntity;
-import com.st0x0ef.stellaris.common.blocks.entities.machines.oxygen.OxygenPropagatorBlockEntity;
 import com.st0x0ef.stellaris.common.systems.energy.EnergyApi;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+
+import java.util.function.Supplier;
 
 public class LookupApiRegistry {
     public static void registerEnergy() {
 
-        EnergyApi.registerEnergyBlock(BlocksRegistry.CABLE, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof CableBlockEntity blockEntity1) blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.CABLE_ENTITY, (level, blockPos, blockState, blockEntity, direction)->{
-            if(blockEntity instanceof CableBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);;
-            return null;
-        });
+        registerToEnergyApi(BlocksRegistry.CABLE, BlockEntityRegistry.CABLE_ENTITY);
+        registerToEnergyApi(BlocksRegistry.OXYGEN_PROPAGATOR, BlockEntityRegistry.OXYGEN_PROPAGATOR);
+        registerToEnergyApi(BlocksRegistry.OXYGEN_DISTRIBUTOR, BlockEntityRegistry.OXYGEN_DISTRIBUTOR);
+        registerToEnergyApi(BlocksRegistry.WATER_SEPARATOR, BlockEntityRegistry.WATER_SEPARATOR_ENTITY);
+        registerToEnergyApi(BlocksRegistry.FUEL_REFINERY, BlockEntityRegistry.FUEL_REFINERY);
+        registerToEnergyApi(BlocksRegistry.SOLAR_PANEL, BlockEntityRegistry.SOLAR_PANEL);
+        registerToEnergyApi(BlocksRegistry.COAL_GENERATOR, BlockEntityRegistry.COAL_GENERATOR);
+        registerToEnergyApi(BlocksRegistry.RADIOACTIVE_GENERATOR, BlockEntityRegistry.RADIOACTIVE_GENERATOR);
+    }
 
-        EnergyApi.registerEnergyBlock(BlocksRegistry.OXYGEN_PROPAGATOR, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof OxygenPropagatorBlockEntity blockEntity1) blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
+    private static void registerToEnergyApi(Supplier<Block> energyBlock, Supplier<BlockEntityType<?>> energyBlockEntity) {
+        EnergyApi.registerEnergyBlock(energyBlock, (level, blockPos, blockState, blockEntity, direction) -> {
+            if(blockEntity instanceof WrappedEnergyBlockEntity blockEntity1) return blockEntity1.getWrappedEnergyContainer();
             return null;
         });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.OXYGEN_PROPAGATOR, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof OxygenPropagatorBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
+        EnergyApi.registerEnergyBlockEntity(energyBlockEntity, (level, blockPos, blockState, blockEntity, direction) -> {
+            if(blockEntity instanceof WrappedEnergyBlockEntity blockEntity1) return blockEntity1.getWrappedEnergyContainer();
             return null;
         });
-
-        EnergyApi.registerEnergyBlock(BlocksRegistry.OXYGEN_DISTRIBUTOR, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof OxygenDistributorBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.OXYGEN_DISTRIBUTOR, (level, blockPos, blockState, blockEntity, direction)->{
-            if(blockEntity instanceof OxygenDistributorBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-
-        EnergyApi.registerEnergyBlock(BlocksRegistry.WATER_SEPARATOR, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof WaterSeparatorBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.WATER_SEPARATOR_ENTITY, (level, blockPos, blockState, blockEntity, direction)->{
-            if(blockEntity instanceof WaterSeparatorBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-
-        EnergyApi.registerEnergyBlock(BlocksRegistry.FUEL_REFINERY, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof FuelRefineryBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.FUEL_REFINERY, (level, blockPos, blockState, blockEntity, direction) -> {
-            if(blockEntity instanceof FuelRefineryBlockEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-
-
-        EnergyApi.registerEnergyBlock(BlocksRegistry.SOLAR_PANEL, (level, blockPos, blockState, blockEntity, direction)-> {
-            if(blockEntity instanceof SolarPanelEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.SOLAR_PANEL, (level, blockPos, blockState, blockEntity, direction)->{
-            if(blockEntity instanceof SolarPanelEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-
-        EnergyApi.registerEnergyBlock(BlocksRegistry.COAL_GENERATOR, (level, blockPos, blockState, blockEntity, direction)-> {
-            if(blockEntity instanceof CoalGeneratorEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.COAL_GENERATOR, (level, blockPos, blockState, blockEntity, direction)->{
-            if(blockEntity instanceof CoalGeneratorEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-
-        EnergyApi.registerEnergyBlock(BlocksRegistry.RADIOACTIVE_GENERATOR, (level, blockPos, blockState, blockEntity, direction)-> {
-            if(blockEntity instanceof RadioactiveGeneratorEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-        EnergyApi.registerEnergyBlockEntity(BlockEntityRegistry.RADIOACTIVE_GENERATOR, (level, blockPos, blockState, blockEntity, direction) ->{
-            if(blockEntity instanceof RadioactiveGeneratorEntity blockEntity1) return blockEntity1.getEnergyStorage(level, blockPos, blockState, blockEntity, direction);
-            return null;
-        });
-
     }
 
 }
