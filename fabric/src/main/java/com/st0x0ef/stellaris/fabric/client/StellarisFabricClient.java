a002119e4037c2104721a26fabda4c03859ee3d6
VoxelShape <mimiccube664@gmail.com>
2024-07-12 16:36:08 +0900

    Fix FabricClient

--- fabric/src/main/java/com/st0x0ef/stellaris/fabric/client/StellarisFabricClient.java
+++ fabric/src/main/java/com/st0x0ef/stellaris/fabric/client/StellarisFabricClient.java
@@ -1,7 +1,6 @@
 package com.st0x0ef.stellaris.fabric.client;
 
 import com.st0x0ef.stellaris.client.StellarisClient;
-import com.st0x0ef.stellaris.fabric.client.renderer.SkyRendererFabric;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 
@@ -12,6 +11,6 @@ public class StellarisFabricClient implements ClientModInitializer {
         StellarisClient.registerEntityRenderer();
         StellarisClient.registerEntityModelLayer();
 
-        WorldRenderEvents.AFTER_SETUP.register(SkyRendererFabric::RegisterSkyRenderer);
+        WorldRenderEvents.AFTER_SETUP.register(FabricSkyRenderer::renderSky);
     }
 }
