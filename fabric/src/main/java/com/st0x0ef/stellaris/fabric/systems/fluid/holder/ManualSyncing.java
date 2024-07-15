be83db83e61752973e6ab751efffa3c3be3e6257
Fej1Dev <guymoshegg007@gmail.com>
2024-07-15 13:59:40 +0300

    start fluid from scratch

--- fabric/src/main/java/com/st0x0ef/stellaris/fabric/systems/fluid/holder/ManualSyncing.java
+++
@@ -1,9 +0,0 @@
-package com.st0x0ef.stellaris.fabric.systems.fluid.holder;
-
-import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
-
-@SuppressWarnings("UnstableApiUsage")
-public interface ManualSyncing {
-    default void setChanged(TransactionContext transaction) {
-    }
-}
