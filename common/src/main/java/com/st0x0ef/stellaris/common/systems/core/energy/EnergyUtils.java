bc916f5a182e84b5f21d7d41afdebf947a82d647
Fej1Dev <guymoshegg007@gmail.com>
2024-07-21 01:13:39 +0300

    better EnergyUtils

--- common/src/main/java/com/st0x0ef/stellaris/common/systems/core/energy/EnergyUtils.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/systems/core/energy/EnergyUtils.java
@@ -1,6 +1,7 @@
 package com.st0x0ef.stellaris.common.systems.core.energy;
 
 import com.st0x0ef.stellaris.common.systems.core.storage.base.ValueStorage;
+import com.st0x0ef.stellaris.common.systems.core.storage.util.TransferUtil;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.entity.BlockEntity;
 
@@ -10,18 +11,7 @@ public class EnergyUtils {
         Direction.stream().forEach((direction)-> { /**this should work*/
             ValueStorage toEnergy = EnergyApi.BLOCK.find(blockEntity,direction);
             ValueStorage fromEnergy = blockEntity.getEnergy(null);
-            if (toEnergy == null || fromEnergy == null) return;
-            if (blockEntity.getEnergy(null).getStoredAmount()!=0) return;
-            long simAmountInsert = toEnergy.insert(amount,true);
-            long simAmountExtract = fromEnergy.extract(amount,true);
-
-            if (simAmountInsert>=simAmountExtract) {
-                fromEnergy.extract(simAmountExtract,false);
-                toEnergy.insert(simAmountExtract,false);
-            } else {
-                fromEnergy.extract(simAmountInsert,false);
-                toEnergy.insert(simAmountInsert,false);
-            }
+            if (fromEnergy!=null && toEnergy!=null) TransferUtil.moveValue(fromEnergy, toEnergy, amount,false);
         });
     }
 }
