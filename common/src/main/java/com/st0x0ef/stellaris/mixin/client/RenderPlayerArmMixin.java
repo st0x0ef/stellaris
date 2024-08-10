6b91087fe1cc96cbbf66649d136293a99a7f2ce4
Ethan Costa <67022946+TathanDev@users.noreply.github.com>
2024-08-10 12:51:27 +0200

    Remove logs

--- common/src/main/java/com/st0x0ef/stellaris/mixin/client/RenderPlayerArmMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/client/RenderPlayerArmMixin.java
@@ -66,7 +66,6 @@ public abstract class RenderPlayerArmMixin extends LivingEntityRenderer<Abstract
     @Inject(method = "render", at = @At("HEAD"), cancellable = true)
     public void renderPlayer(AbstractClientPlayer entity, float entityYaw, float partialTicks, PoseStack poseStack, MultiBufferSource buffer, int packedLight, CallbackInfo ci) {
         if (entity.getVehicle() instanceof LanderEntity) {
-            Stellaris.LOG.error("not rendering");
             ci.cancel();
         }
     }
