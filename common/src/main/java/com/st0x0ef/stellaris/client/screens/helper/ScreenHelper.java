640cd9486c1f17dfad5c6654decab5a3976f9755
st0x0ef <75510490+st0x0ef@users.noreply.github.com>
2024-08-14 14:52:26 -0400

    fix crash on planet selection screen

--- common/src/main/java/com/st0x0ef/stellaris/client/screens/helper/ScreenHelper.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/screens/helper/ScreenHelper.java
@@ -171,7 +171,7 @@ public class ScreenHelper {
         poseStack.pushPose();
 
         poseStack.translate(x + width / 2.0f, y + height / 2.0f, 0);
-        poseStack.mulPose(new Quaternionf((Quaternionfc) new Matrix4f().rotation(rotationAngle, 0.0f, 0.0f, 1.0f)));
+        poseStack.mulPose(new Quaternionf(0.0f, 0.0f, 1.0f, 1.0f).rotateZ(rotationAngle));
         poseStack.translate(-(x + width / 2.0f), -(y + height / 2.0f), 0);
 
         RenderSystem.setShaderTexture(0, resourceLocation);
