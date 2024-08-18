510041dc19e619bc1898a9a2347da11c4ff52487
st0x0ef <75510490+st0x0ef@users.noreply.github.com>
2024-08-18 09:36:34 -0400

    fix fire texture in coal generator/radioactive generator

--- common/src/main/java/com/st0x0ef/stellaris/client/screens/GUISprites.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/screens/GUISprites.java
@@ -16,6 +16,6 @@ public class GUISprites {
     public static final ResourceLocation FUEL_OVERLAY = new ResourceLocation(Stellaris.MODID, "textures/gui/util/fuel_overlay.png");
     public static final ResourceLocation BATTERY_OVERLAY = new ResourceLocation(Stellaris.MODID, "textures/gui/util/battery_overlay.png");
     public static final ResourceLocation ENERGY_FULL = new ResourceLocation(Stellaris.MODID, "textures/gui/util/energy_full.png");
-    public static final ResourceLocation LIT_PROGRESS_SPRITE = new ResourceLocation(Stellaris.MODID, "textures/gui/util/fire_full.png");
+    public static final ResourceLocation LIT_PROGRESS_SPRITE = new ResourceLocation(Stellaris.MODID, "util/fire_full");
     public static final ResourceLocation FLUID_TANK_OVERLAY = new ResourceLocation(Stellaris.MODID, "textures/gui/util/fluid_tank_overlay.png");
 }
