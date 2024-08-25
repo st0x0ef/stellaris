1362b1f1aa373908508275b0de7d924456fcd8d8
eirmax <eirmax8@gmail.com>
2024-08-25 19:03:39 +0700

    Fix trigger before working on achievements

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/AdvancementsRegister.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/AdvancementsRegister.java
@@ -1,8 +1,9 @@
 package com.st0x0ef.stellaris.common.registry;
 
+import com.st0x0ef.stellaris.common.utils.Utils;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementType;
-import net.minecraft.advancements.critereon.InventoryChangeTrigger;
+import net.minecraft.advancements.critereon.ChangeDimensionTrigger;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.advancements.AdvancementSubProvider;
 import net.minecraft.advancements.Advancement.Builder;
@@ -10,17 +11,31 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.ItemLike;
 
+
 import java.util.function.Consumer;
+
 
 public class AdvancementsRegister implements AdvancementSubProvider {
 
 
     public void generate(HolderLookup.Provider registries, Consumer<AdvancementHolder> writer) {
-        AdvancementHolder advancementHolder = Builder.advancement().display((ItemLike) BlocksRegistry.MOON_SAND, Component.translatable("advancements.moon.root.title"), Component.translatable("advancements.moon.root.description"), new ResourceLocation("textures/gui/advancements/backgrounds/stellaris.png"), AdvancementType.TASK, true, true, false).addCriterion("land on the moon", InventoryChangeTrigger.TriggerInstance.hasItems((ItemLike) ItemsRegistry.MOON_SAND_ITEM)).save(writer, "moon/root");
+        AdvancementHolder moonAdvancement = Builder.advancement()
+                .display((ItemLike) BlocksRegistry.MOON_SAND, Component.translatable("advancements.moon.root.title"), Component.translatable("advancements.moon.root.description"),
+                        new ResourceLocation("textures/gui/advancements/backgrounds/stellaris.png"), AdvancementType.TASK, true, true, false)
+                .addCriterion("land_on_the_moon", ChangeDimensionTrigger.TriggerInstance.changedDimensionTo(Utils.getPlanetLevel(new ResourceLocation("moon"))))
+                .save(writer, "moon/root");
 
-        AdvancementHolder advancementHolder2 = Builder.advancement().display((ItemLike) BlocksRegistry.MARS_SAND, Component.translatable("advancements.mars.root.title"), Component.translatable("advancements.mars.root.description"), new ResourceLocation("textures/gui/advancements/backgrounds/stellaris.png"), AdvancementType.TASK, true, true, false).addCriterion("land on the mars", InventoryChangeTrigger.TriggerInstance.hasItems((ItemLike) ItemsRegistry.MARS_SAND)).save(writer, "mars/root");
+        AdvancementHolder marsAdvancement = Builder.advancement()
+                .display((ItemLike) BlocksRegistry.MARS_SAND, Component.translatable("advancements.mars.root.title"), Component.translatable("advancements.mars.root.description"),
+                        null, AdvancementType.TASK, true, true, false)
+                .addCriterion("land_on_the_mars", ChangeDimensionTrigger.TriggerInstance.changedDimensionTo(Utils.getPlanetLevel(new ResourceLocation("mars"))))
+                .save(writer, "mars/root");
 
-
+        AdvancementHolder venusAdvancement = Builder.advancement()
+                .display((ItemLike) BlocksRegistry.VENUS_SAND, Component.translatable("advancements.venus.root.title"), Component.translatable("advancements.venus.root.description"),
+                        null, AdvancementType.TASK, true, true, false)
+                .addCriterion("land_on_the_mars", ChangeDimensionTrigger.TriggerInstance.changedDimensionTo(Utils.getPlanetLevel(new ResourceLocation("venus"))))
+                .save(writer, "venus/root");
 
 
 
