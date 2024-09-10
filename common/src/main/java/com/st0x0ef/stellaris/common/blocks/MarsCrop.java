cb58160723a645c44a86bf9b1e8fd6e76446d5f1
TATHAN <costa.tathan@gmail.com>
2024-09-10 22:43:38 +0200

    Finish Mars fruit

---
+++ common/src/main/java/com/st0x0ef/stellaris/common/blocks/MarsCrop.java
@@ -0,0 +1,64 @@
+package com.st0x0ef.stellaris.common.blocks;
+
+import com.st0x0ef.stellaris.common.registry.BlocksRegistry;
+import com.st0x0ef.stellaris.common.registry.ItemsRegistry;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.ItemLike;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.CropBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+public class MarsCrop extends CropBlock {
+
+    private static final VoxelShape[] SHAPE_BY_AGE = new VoxelShape[]{Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 3.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 4.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 5.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 6.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 7.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0), Block.box(0.0, 0.0, 0.0, 16.0, 9.0, 16.0)};
+
+    public static final int MAX_AGE = 7;
+    public static final IntegerProperty AGE = IntegerProperty.create("age", 0, 7);
+
+    public MarsCrop(Properties properties) {
+        super(properties);
+    }
+
+    @Override
+    protected boolean canSurvive(BlockState state, LevelReader level, BlockPos pos) {
+        BlockPos blockpos = pos.below();
+        BlockState belowBlockState = level.getBlockState(blockpos);
+        return CropBlock.hasSufficientLight(level, pos) && belowBlockState.is(BlocksRegistry.MARS_SAND);
+    }
+
+    @Override
+    protected ItemLike getBaseSeedId() {
+        return ItemsRegistry.MARS_FRUIT.get();
+    }
+
+    @Override
+    public IntegerProperty getAgeProperty() {
+        return AGE;
+    }
+
+    @Override
+    public int getMaxAge() {
+        return MAX_AGE;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE_BY_AGE[getAge(state)];
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
+        pBuilder.add(AGE);
+    }
+
+    protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
+        return state.is(BlocksRegistry.MARS_SAND);
+    }
+
+}
