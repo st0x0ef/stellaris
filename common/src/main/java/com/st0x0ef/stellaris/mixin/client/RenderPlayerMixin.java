b6ead205e82aae6b847c7e75d31e6deb990ebe48
TATHAN <costa.tathan@gmail.com>
2024-09-16 17:05:35 +0200

    Add space suit arm

--- common/src/main/java/com/st0x0ef/stellaris/mixin/client/RenderPlayerMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/client/RenderPlayerMixin.java
@@ -1,10 +1,16 @@
 package com.st0x0ef.stellaris.mixin.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.client.renderers.armors.JetSuitModel;
+import com.st0x0ef.stellaris.client.renderers.armors.SpaceSuitModel;
+import com.st0x0ef.stellaris.common.armors.AbstractSpaceArmor;
 import com.st0x0ef.stellaris.common.armors.JetSuit;
 import com.st0x0ef.stellaris.common.entities.vehicles.LanderEntity;
+import com.st0x0ef.stellaris.common.items.CustomArmorItem;
+import com.st0x0ef.stellaris.common.registry.ItemsRegistry;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
@@ -36,31 +42,46 @@ public abstract class RenderPlayerMixin extends LivingEntityRenderer<AbstractCli
     @Inject(method = "renderHand", at = @At("HEAD"), cancellable = true)
     private void renderPlayerHand(PoseStack poseStack, MultiBufferSource buffer, int packedLight, AbstractClientPlayer player, ModelPart renderedArm, ModelPart rendererArmwear, CallbackInfo ci) {
         ItemStack stack = player.getItemBySlot(EquipmentSlot.CHEST);
-        if (!(stack.getItem() instanceof JetSuit.Suit spaceSuit)) return;
-        ci.cancel();
 
-        PlayerModel<AbstractClientPlayer> playerModel = getModel();
-        setModelProperties(player);
-        playerModel.attackTime = 0.0F;
-        playerModel.crouching = false;
-        playerModel.swimAmount = 0.0F;
-        playerModel.setupAnim(player, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
-        renderedArm.xRot = 0.0F;
+        if(stack.getItem() instanceof JetSuit.Suit || (stack.getItem() instanceof AbstractSpaceArmor)) {
+            Stellaris.LOG.error("eee");
+            ci.cancel();
 
-        ModelLayerLocation layer = JetSuitModel.LAYER_LOCATION;
-        ResourceLocation  texture = JetSuitModel.TEXTURE;
+            PlayerModel<AbstractClientPlayer> playerModel = getModel();
+            setModelProperties(player);
+            playerModel.attackTime = 0.0F;
+            playerModel.crouching = false;
+            playerModel.swimAmount = 0.0F;
+            playerModel.setupAnim(player, 0.0F, 0.0F, 0.0F, 0.0F, 0.0F);
+            renderedArm.xRot = 0.0F;
 
-        ModelPart rootPart = Minecraft.getInstance().getEntityModels().bakeLayer(layer);
 
-        JetSuitModel spaceSuitModel = new JetSuitModel(rootPart, EquipmentSlot.CHEST, stack, null);
-        boolean isRightHand = (renderedArm == spaceSuitModel.rightArm);
+            ModelLayerLocation layer;
+            ResourceLocation  texture = null;
+            HumanoidModel<?> model = null;
+            ModelPart rootPart;
 
-        if (isRightHand) {
-            spaceSuitModel.rightArm.copyFrom(renderedArm);
-            spaceSuitModel.rightArm.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(texture)), packedLight, OverlayTexture.NO_OVERLAY);
-        } else {
-            spaceSuitModel.leftArm.copyFrom(renderedArm);
-            spaceSuitModel.leftArm.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(texture)), packedLight, OverlayTexture.NO_OVERLAY);
+            if(stack.getItem() instanceof JetSuit.Suit) {
+                layer = JetSuitModel.LAYER_LOCATION;
+                texture = JetSuitModel.TEXTURE;
+                rootPart = Minecraft.getInstance().getEntityModels().bakeLayer(layer);
+                model = new JetSuitModel(rootPart, EquipmentSlot.CHEST, stack, null);
+            } else if (stack.getItem() instanceof AbstractSpaceArmor){
+                layer = SpaceSuitModel.LAYER_LOCATION;
+                rootPart = Minecraft.getInstance().getEntityModels().bakeLayer(layer);
+                texture = SpaceSuitModel.TEXTURE;
+                model = new SpaceSuitModel(rootPart, EquipmentSlot.CHEST, stack, null);
+
+            }
+            boolean isRightHand = (renderedArm == model.rightArm);
+
+            if (isRightHand) {
+                model.rightArm.copyFrom(renderedArm);
+                model.rightArm.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(texture)), packedLight, OverlayTexture.NO_OVERLAY);
+            } else {
+                model.leftArm.copyFrom(renderedArm);
+                model.leftArm.render(poseStack, buffer.getBuffer(RenderType.entityTranslucent(texture)), packedLight, OverlayTexture.NO_OVERLAY);
+            }
         }
     }
 
