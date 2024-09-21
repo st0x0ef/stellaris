74d3ebe21b7ed48837af94dd3246fff5f9f3d14a
TATHAN <costa.tathan@gmail.com>
2024-09-21 17:32:38 +0200

    Change stellaris commands

--- common/src/main/java/com/st0x0ef/stellaris/common/commands/StellarisCommands.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/commands/StellarisCommands.java
@@ -14,40 +14,43 @@ public class StellarisCommands {
     public StellarisCommands(CommandDispatcher<CommandSourceStack> dispatcher) {
 
         dispatcher.register(Commands.literal("stellaris")
-                .then(Commands.literal("planetScreen")
-                        .requires(c -> c.hasPermission(2))
-                        .executes((CommandContext<CommandSourceStack> context) -> {
-                            PlanetUtil.openPlanetSelectionMenu(context.getSource().getPlayer(), true);
-                            return 0;
-                        }))
                 .then(Commands.literal("oil")
                         .requires(c -> c.hasPermission(2))
-                        .executes((CommandContext<CommandSourceStack> context) -> {
-                            ChunkAccess access = context.getSource().getPlayer().level().getChunk(context.getSource().getPlayer().getOnPos());
-                            context.getSource().getPlayer().sendSystemMessage(Component.literal("Oil Level : " + access.stellaris$getChunkOilLevel()));
-                            return 0;
-                        }))
-                .then(Commands.literal("setOil")
-                        .requires(c -> c.hasPermission(2))
-                        .then(Commands.argument("level", IntegerArgumentType.integer())
+                        .then(Commands.literal("set")
+                                .then(Commands.argument("level", IntegerArgumentType.integer())
+                                        .executes((CommandContext<CommandSourceStack> context) -> {
+                                            ChunkAccess access = context.getSource().getPlayer().level().getChunk(context.getSource().getPlayer().getOnPos());
+                                            access.stellaris$setChunkOilLevel(context.getArgument("level", Integer.class));
+                                            context.getSource().getPlayer().sendSystemMessage(Component.literal("Oil Level : " + access.stellaris$getChunkOilLevel()));
+                                            return 0;
+                                        })))
+                        .then(Commands.literal("get")
                                 .executes((CommandContext<CommandSourceStack> context) -> {
                                     ChunkAccess access = context.getSource().getPlayer().level().getChunk(context.getSource().getPlayer().getOnPos());
-                                    access.stellaris$setChunkOilLevel(context.getArgument("level", Integer.class));
                                     context.getSource().getPlayer().sendSystemMessage(Component.literal("Oil Level : " + access.stellaris$getChunkOilLevel()));
                                     return 0;
                                 })))
-                .then(Commands.literal("galaxyScreen")
+
+                .then(Commands.literal("screen")
                         .requires(c -> c.hasPermission(2))
-                        .executes((CommandContext<CommandSourceStack> context) -> {
-                            PlanetUtil.openMilkyWayMenu(context.getSource().getPlayer());
-                            return 0;
-                        }))
-                .then(Commands.literal("waitScreen")
-                        .requires(c -> c.hasPermission(2))
-                        .executes((CommandContext<CommandSourceStack> context) -> {
-                            PlanetUtil.openWaitMenu(context.getSource().getPlayer(), context.getSource().getPlayer().getDisplayName().getString());
-                            return 0;
-                        }))
+                        .then(Commands.literal("galaxyScreen")
+                                .executes((CommandContext<CommandSourceStack> context) -> {
+                                    PlanetUtil.openMilkyWayMenu(context.getSource().getPlayer());
+                                    return 0;
+                                }))
+                        .then(Commands.literal("waitScreen")
+                                .executes((CommandContext<CommandSourceStack> context) -> {
+                                    PlanetUtil.openWaitMenu(context.getSource().getPlayer(), context.getSource().getPlayer().getDisplayName().getString());
+                                    return 0;
+                                }))
+                        .then(Commands.literal("planetScreen")
+                                .executes((CommandContext<CommandSourceStack> context) -> {
+                                    PlanetUtil.openPlanetSelectionMenu(context.getSource().getPlayer(), true);
+                                    return 0;
+                                }))
+                )
+
+
         );
     }
 }
