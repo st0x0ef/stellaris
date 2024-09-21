56410174b3802327119bcd8f07e7107af09232c2
TATHAN <costa.tathan@gmail.com>
2024-09-21 17:04:48 +0200

    added pumpjack crafts and polish

--- common/src/main/java/com/st0x0ef/stellaris/common/network/packets/SyncWidgetsTanksPacket.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/network/packets/SyncWidgetsTanksPacket.java
@@ -1,6 +1,8 @@
 package com.st0x0ef.stellaris.common.network.packets;
 
 import com.st0x0ef.stellaris.common.blocks.entities.machines.FuelRefineryBlockEntity;
+import com.st0x0ef.stellaris.common.blocks.entities.machines.PumpjackBlockEntity;
+import com.st0x0ef.stellaris.common.blocks.entities.machines.WaterPumpBlockEntity;
 import com.st0x0ef.stellaris.common.blocks.entities.machines.WaterSeparatorBlockEntity;
 import com.st0x0ef.stellaris.common.menus.*;
 import com.st0x0ef.stellaris.common.network.NetworkRegistry;
@@ -88,6 +90,15 @@ public class SyncWidgetsTanksPacket implements CustomPacketPayload {
                     blockEntity.getWrappedEnergyContainer().setEnergy(syncWidgetsTanks.component[0]);
                 }
             }
+            case WaterPumpMenu menu -> {
+                WaterPumpBlockEntity blockEntity = menu.getBlockEntity();
+                if (syncWidgetsTanks.component.length == 1 && syncWidgetsTanks.locations.length == 1) {
+                    blockEntity.getWaterTank().setFluid(BuiltInRegistries.FLUID.get(syncWidgetsTanks.locations[0]), syncWidgetsTanks.component[0]);
+                }
+                else if (syncWidgetsTanks.component.length == 2) {
+                    blockEntity.getWrappedEnergyContainer().setEnergy(syncWidgetsTanks.component[0]);
+                }
+            }
             case SolarPanelMenu menu -> menu.getEnergyContainer().setEnergy(syncWidgetsTanks.component[0]);
             case CoalGeneratorMenu menu ->
                     menu.getBlockEntity().getWrappedEnergyContainer().setEnergy(syncWidgetsTanks.component[0]);
@@ -96,6 +107,17 @@ public class SyncWidgetsTanksPacket implements CustomPacketPayload {
             case OxygenGeneratorMenu menu -> {
                     menu.getBlockEntity().getWrappedEnergyContainer().setEnergy(syncWidgetsTanks.component[0]);
                     menu.getBlockEntity().oxygenTank.setAmount(syncWidgetsTanks.component[1]);
+            }
+            case PumpjackMenu menu -> {
+                PumpjackBlockEntity blockEntity = menu.getBlockEntity();
+                if (syncWidgetsTanks.component.length == 2 && syncWidgetsTanks.locations.length == 1) {
+                    blockEntity.resultTank.setFluid(BuiltInRegistries.FLUID.get(syncWidgetsTanks.locations[0]), syncWidgetsTanks.component[0]);
+                    blockEntity.chunkOilLevel = (int) syncWidgetsTanks.component[1];
+                } else {
+                    menu.getBlockEntity().getWrappedEnergyContainer().setEnergy(syncWidgetsTanks.component[0]);
+
+                }
+
             }
             default -> {
             }
