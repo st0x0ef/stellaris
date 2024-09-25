069d1e761ccb53f8aed82a830bd83e963cdc1573
TATHAN <costa.tathan@gmail.com>
2024-09-25 17:33:01 +0200

    Right click with tank to fill space suit

--- common/src/main/java/com/st0x0ef/stellaris/common/items/OxygenTankItem.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/items/OxygenTankItem.java
@@ -1,13 +1,21 @@
 package com.st0x0ef.stellaris.common.items;
 
+import com.st0x0ef.stellaris.Stellaris;
+import com.st0x0ef.stellaris.common.data_components.JetSuitComponent;
+import com.st0x0ef.stellaris.common.data_components.OxygenComponent;
 import com.st0x0ef.stellaris.common.registry.DataComponentsRegistry;
 import com.st0x0ef.stellaris.common.utils.OxygenUtils;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 
 import java.util.List;
 
@@ -21,6 +29,37 @@ public class OxygenTankItem extends Item {
         if (stack.has(DataComponentsRegistry.STORED_OXYGEN_COMPONENT.get())) {
             tooltip.add(Component.translatable("tooltip.item.stellaris.oxygen_tank", OxygenUtils.getOxygen(stack), OxygenUtils.getOxygenCapacity(stack)).withStyle(ChatFormatting.GRAY));
         }
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
+        if(level.isClientSide) return super.use(level, player, usedHand);
+
+        if (player.isShiftKeyDown()) {
+            if (player.getItemBySlot(EquipmentSlot.CHEST).has(DataComponentsRegistry.STORED_OXYGEN_COMPONENT.get())) {
+                ItemStack armor = player.getItemBySlot(EquipmentSlot.CHEST);
+                Stellaris.LOG.info("ee");
+
+                ItemStack tank = player.getItemInHand(usedHand);
+                OxygenComponent oxygenComponent = tank.get(DataComponentsRegistry.STORED_OXYGEN_COMPONENT.get());
+
+                if (oxygenComponent.oxygen() == 0) {
+                    return super.use(level, player, usedHand);
+                }
+
+                if (OxygenUtils.getOxygenCapacity(armor) - OxygenUtils.getOxygen(armor) > oxygenComponent.oxygen()) {
+                    OxygenUtils.addOxygen(armor, oxygenComponent.oxygen());
+                    OxygenUtils.setOxygen(tank, 0);
+                } else if (OxygenUtils.getOxygenCapacity(armor) - OxygenUtils.getOxygen(armor) <= oxygenComponent.oxygen()) {
+                    OxygenUtils.addOxygen(armor, OxygenUtils.getOxygenCapacity(armor) - OxygenUtils.getOxygen(armor));
+                    OxygenUtils.addOxygen(tank, -(OxygenUtils.getOxygenCapacity(armor) + OxygenUtils.getOxygen(armor)));
+                }
+
+            }
+        }
+
+        return super.use(level, player, usedHand);
+
     }
 
     @Override
