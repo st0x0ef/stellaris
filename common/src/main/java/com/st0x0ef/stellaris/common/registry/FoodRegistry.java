ff9dee52f237d3ab906df6604432a3093bf0ba38
eirmax <eirmax8@gmail.com>
2024-09-28 12:59:04 +0700

    animated fix

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/FoodRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/FoodRegistry.java
@@ -7,5 +7,5 @@ import net.minecraft.world.food.FoodProperties;
 public class FoodRegistry {
     public static final FoodProperties CHEESE = new FoodProperties.Builder().nutrition(3).saturationModifier(1).fast().build();
     public static final FoodProperties COSMO_BREAD = (new FoodProperties.Builder()).nutrition(4).saturationModifier(0.4F).build();
-    public static final FoodProperties COSMO_COFFEE = (new FoodProperties.Builder()).nutrition(5).saturationModifier(0.3F).fast().effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 100, 0), 0.3F).build();
+    public static final FoodProperties COSMO_COFFEE = (new FoodProperties.Builder()).nutrition(5).saturationModifier(0.3F).fast().effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1260, 0), 0.3F).build();
 }
