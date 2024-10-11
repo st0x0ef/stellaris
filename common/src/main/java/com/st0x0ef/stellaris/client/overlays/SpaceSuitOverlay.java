02f8b6a00ce2cf9a2c728d4e9ab7de67a55287a6
Fej1Dev <guymoshegg007@gmail.com>
2024-10-11 18:12:11 +0300

    add oxygen gauge for space suit

--- common/src/main/java/com/st0x0ef/stellaris/client/overlays/SpaceSuitOverlay.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/overlays/SpaceSuitOverlay.java
@@ -2,12 +2,14 @@ package com.st0x0ef.stellaris.client.overlays;
 
 import com.st0x0ef.stellaris.client.screens.GUISprites;
 import com.st0x0ef.stellaris.common.items.armors.SpaceSuit;
+import com.st0x0ef.stellaris.common.utils.OxygenUtils;
 import com.st0x0ef.stellaris.common.utils.Utils;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
@@ -24,16 +26,22 @@ public class SpaceSuitOverlay {
         if (player!=null) {
             if (Utils.isLivingInSpaceSuit(player)) {
                 ItemStack itemStack = player.getItemBySlot(EquipmentSlot.CHEST);
-
-                //we can add default gui stuff for the space suit here
-                graphics.blit(GUISprites.SPACESUIT_OXYGEN_BAR, 5, 5, 0, 0, 37, 10, 37, 10);
-                //graphics.blitSprite(SPRITE, getWidth(), getHeight(), 0, getHeight() - i, getX(), getY() + getHeight() - i, getWidth(), i);
-
-                //first layer is 1 since what we add is 0
                 if (itemStack.getItem() instanceof SpaceSuit spaceSuit) {
+
+
+                    //We can add default gui stuff for the space suit here
+                    graphics.blit(GUISprites.SPACESUIT_OXYGEN_BAR, 5, 5, 0, 0, 37, 10, 37, 10);
+
+                    int i = Mth.ceil(Mth.clamp((float) OxygenUtils.getOxygen(itemStack) / (float) OxygenUtils.getOxygenCapacity(itemStack),
+                            0.0F, 1.0F) * (24 - 1));
+                    graphics.blitSprite(GUISprites.SPACESUIT_FULL_BAR_SPRITE, 24, 4, 0, 0, 15, 8, i, 4);
+
+
+                    //Logic for modules
                     AtomicInteger y = new AtomicInteger(37);
                     spaceSuit.getModules(itemStack).forEach(module -> {
-                        y.set(module.renderStackedGui(graphics, deltaTracker, player, itemStack, y.get())+1);
+                        int y1 = module.renderStackedGui(graphics, deltaTracker, player, itemStack, y.get());
+                        if (y1 != y.get()) y.set(y1+1);
                         module.renderToGui(graphics, deltaTracker, player, itemStack);
                     });
                     y.set(37);
