5baba3434ad3e3fc7538f660b62d1bc799182ca3
eirmax <eirmax8@gmail.com>
2024-10-15 21:35:03 +0700

    // commit back

--- common/src/main/java/com/st0x0ef/stellaris/common/events/Events.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/events/Events.java
@@ -38,7 +38,7 @@ public class Events {
                             .orElse(0);
 
                     if (maxRadiationLevel > 0) {
-                        player.addEffect(new MobEffectInstance(EffectsRegistry.RADIOACTIVE, 100, maxRadiationLevel - 1));
+                       // player.addEffect(new MobEffectInstance(EffectsRegistry.RADIOACTIVE, 100, maxRadiationLevel - 1));
 
                         player.addEffect(new MobEffectInstance(MobEffects.CONFUSION, 80));
                         player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 80));
