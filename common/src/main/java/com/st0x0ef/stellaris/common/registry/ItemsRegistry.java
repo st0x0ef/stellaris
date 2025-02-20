43d8661e6ff8385226b0a604c227af3e1f2a7cee
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-02-20 11:19:11 -0500

    fix data component

--- common/src/main/java/com/st0x0ef/stellaris/common/registry/ItemsRegistry.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/registry/ItemsRegistry.java
@@ -1,5 +1,6 @@
 package com.st0x0ef.stellaris.common.registry;
 
+import com.fej1fun.potentials.fluid.ItemFluidStorage;
 import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.client.renderers.entities.vehicle.rocket.RocketModel;
 import com.st0x0ef.stellaris.client.screens.GUISprites;
@@ -360,8 +361,8 @@ public class ItemsRegistry {
      * Oxygen
      */
 
-    public static final RegistrySupplier<Item> OXYGEN_TANK = ITEMS.register("oxygen_tank", () -> new OxygenTankItem(new Item.Properties().arch$tab(CreativeTabsRegistry.STELLARIS_TAB), 3600));
-    public static final RegistrySupplier<Item> BIG_OXYGEN_TANK = ITEMS.register("big_oxygen_tank", () -> new OxygenTankItem(new Item.Properties().arch$tab(CreativeTabsRegistry.STELLARIS_TAB), 10800));
+    public static final RegistrySupplier<Item> OXYGEN_TANK = ITEMS.register("oxygen_tank", () -> new OxygenTankItem(new Item.Properties().arch$tab(CreativeTabsRegistry.STELLARIS_TAB).component(DataComponentsRegistry.FLUID_LIST.get(), ItemFluidStorage.getEmpty(1)), 3600)); // we need potentials 0.4.1 to use this method
+    public static final RegistrySupplier<Item> BIG_OXYGEN_TANK = ITEMS.register("big_oxygen_tank", () -> new OxygenTankItem(new Item.Properties().arch$tab(CreativeTabsRegistry.STELLARIS_TAB).component(DataComponentsRegistry.FLUID_LIST.get(), ItemFluidStorage.getEmpty(1)), 10800));
 
     /**
      * Suit
