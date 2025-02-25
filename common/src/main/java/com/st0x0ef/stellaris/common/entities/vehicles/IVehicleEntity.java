400ac8178a242ad1b3b595e50168f5ec3b748d4b
lexiccn <13711345+lexiccn@users.noreply.github.com>
2025-02-25 14:54:55 +0000

    Add FUEL_TYPE to VehicleEntity interface

--- common/src/main/java/com/st0x0ef/stellaris/common/entities/vehicles/IVehicleEntity.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/entities/vehicles/IVehicleEntity.java
@@ -1,5 +1,6 @@
 package com.st0x0ef.stellaris.common.entities.vehicles;
 
+import com.st0x0ef.stellaris.common.vehicle_upgrade.FuelType;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.protocol.Packet;
@@ -21,6 +22,8 @@ import net.minecraft.world.phys.Vec3;
 
 public abstract class IVehicleEntity extends Entity{
     public int FUEL;
+
+    public FuelType.Type FUEL_TYPE;
 
     private int lerpSteps;
     private double lerpX;
