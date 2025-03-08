70d8739be20f16b76b7ce3c37c80893eff1e40a6
eirmax <eirmax8@gmail.com>
2025-03-08 20:36:05 +0700

    jetsuit rename

--- common/src/main/java/com/st0x0ef/stellaris/client/renderers/armors/JetSuitModel.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/renderers/armors/JetSuitModel.java
@@ -17,6 +17,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class JetSuitModel extends HumanoidModel<LivingEntity> {
@@ -27,15 +28,16 @@ public class JetSuitModel extends HumanoidModel<LivingEntity> {
 
 	private final ModelPart head;
 	private final ModelPart body;
-	private final ModelPart leftArm;
-	private final ModelPart rightArm;
+	private final ModelPart left_arm;
+	private final ModelPart right_arm;
 	private final ModelPart waist;
-	private final ModelPart leftLeg;
-	private final ModelPart rightLeg;
-	private final ModelPart leftShoe;
-	private final ModelPart rightShoe;
-	private final ModelPart antenna;
-	private final ModelPart lamp;
+	private final ModelPart left_leg;
+	private final ModelPart right_leg;
+	private final ModelPart left_shoe;
+	private final ModelPart right_shoe;
+	private final ModelPart antenna_r1;
+	private final ModelPart lamp_r1;
+
 
 
 	private final EquipmentSlot slot;
@@ -46,15 +48,15 @@ public class JetSuitModel extends HumanoidModel<LivingEntity> {
 		this.parentModel = parentModel;
 		this.head = root.getChild("head");
 		this.body = root.getChild("body");
-		this.leftArm = root.getChild("left_arm");
-		this.rightArm = root.getChild("right_arm");
+		this.left_arm = root.getChild("left_arm");
+		this.right_arm = root.getChild("right_arm");
 		this.waist = root.getChild("waist");
-		this.leftLeg = root.getChild("left_leg");
-		this.rightLeg = root.getChild("right_leg");
-		this.leftShoe = leftLeg.getChild("left_shoe");
-		this.rightShoe = rightLeg.getChild("right_shoe");
-		this.antenna = head.getChild("antenna_r1");
-		this.lamp = head.getChild("lamp_r1");
+		this.left_leg = root.getChild("left_leg");
+		this.right_leg = root.getChild("right_leg");
+		this.left_shoe = left_leg.getChild("left_shoe");
+		this.right_shoe = right_leg.getChild("right_shoe");
+		this.antenna_r1 = head.getChild("antenna_r1");
+		this.lamp_r1 = head.getChild("lamp_r1");
 
 
 		this.slot = slot;
@@ -81,11 +83,11 @@ public class JetSuitModel extends HumanoidModel<LivingEntity> {
 
 		PartDefinition left_arm = partdefinition.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(0, 34).addBox(9.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.75F))
 				.texOffs(48, 32).addBox(9.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.0F))
-				.texOffs(0, 50).addBox(9.0F, 11.1F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0001F)), PartPose.offset(5.0F, 2.0F, 0.0F));
+				.texOffs(0, 50).addBox(9.0F, 11.1F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0001F)), PartPose.offset(-7.0F, -10.0F, 0.0F));
 
 		PartDefinition right_arm = partdefinition.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(0, 34).mirror().addBox(-13.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.75F)).mirror(false)
 				.texOffs(48, 32).mirror().addBox(-13.0F, -2.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(1.0F)).mirror(false)
-				.texOffs(0, 50).mirror().addBox(-13.0F, 11.1F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0001F)).mirror(false), PartPose.offset(-5.0F, 2.0F, 0.0F));
+				.texOffs(0, 50).mirror().addBox(-13.0F, 11.1F, -2.0F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0001F)).mirror(false), PartPose.offset(3.0F, -10.0F, 0.0F));
 
 		PartDefinition waist = partdefinition.addOrReplaceChild("waist", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, 0.0F, -2.1F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.5F)), PartPose.offset(-2.0F, -12.0F, 0.0F));
 
@@ -95,9 +97,9 @@ public class JetSuitModel extends HumanoidModel<LivingEntity> {
 		PartDefinition right_leg = partdefinition.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(16, 45).addBox(-6.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.5F))
 				.texOffs(0, 50).mirror().addBox(-6.0F, 12.8F, -2.1F, 4.0F, 3.0F, 4.0F, new CubeDeformation(0.0001F)).mirror(false), PartPose.offset(0.0F, 0.0F, 0.0F));
 
-		PartDefinition left_shoe = left_leg.addOrReplaceChild("left_shoe", CubeListBuilder.create().texOffs(32, 45).mirror().addBox(1.9F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.75F)).mirror(false), PartPose.offset(-4.0F, 0.0F, 0.0F));
+		PartDefinition left_shoe = partdefinition.addOrReplaceChild("left_shoe", CubeListBuilder.create().texOffs(32, 45).mirror().addBox(1.9F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.75F)).mirror(false), PartPose.offset(-4.0F, 0.0F, 0.0F));
 
-		PartDefinition right_shoe = right_leg.addOrReplaceChild("right_shoe", CubeListBuilder.create().texOffs(32, 45).addBox(-5.9F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.75F)), PartPose.offset(0.0F, 0.0F, 0.0F));
+		PartDefinition right_shoe = partdefinition.addOrReplaceChild("right_shoe", CubeListBuilder.create().texOffs(32, 45).addBox(-5.9F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.75F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 128, 128);
 	}
@@ -115,46 +117,32 @@ public class JetSuitModel extends HumanoidModel<LivingEntity> {
 	}
 
 	@Override
-	protected  Iterable<ModelPart> headParts() {
-		return ImmutableList.of(head, antenna, lamp);
+	protected @Nullable Iterable<ModelPart> headParts() {
+		return ImmutableList.of(head);
 	}
 
 	@Override
-	protected  Iterable<ModelPart> bodyParts() {
-		return ImmutableList.of(body, rightArm, leftArm, rightLeg, leftLeg, head, leftShoe, rightShoe);
+	protected @Nullable Iterable<ModelPart> bodyParts() {
+		return ImmutableList.of(body, rightArm, leftArm, rightLeg, leftLeg, head, left_shoe, right_shoe);
 	}
-
-	@Override
-	public void setAllVisible(boolean visible) {
-		super.setAllVisible(visible);
-
-		this.leftShoe.visible = visible;
-		this.rightShoe.visible = visible;
-		this.antenna.visible = visible;
-		this.lamp.visible = visible;
-		this.waist.visible = visible;
-	}
-
 
 	private void setVisible() {
 		this.setAllVisible(false);
 		switch (this.slot) {
 			case HEAD -> {
 				this.head.visible = true;
-				this.antenna.visible = true;
-				this.lamp.visible = true;
+				this.antenna_r1.visible = true;
+				this.lamp_r1.visible = true;
 			}
 			case CHEST -> {
 				this.body.visible = true;
 				this.rightArm.visible = true;
 				this.leftArm.visible = true;
+				this.waist.visible = true;
 			}
 			case LEGS -> {
 				this.rightLeg.visible = true;
 				this.leftLeg.visible = true;
-			}
-			case FEET ->  {
-				this.waist.visible = true;
 			}
 		}
 	}
