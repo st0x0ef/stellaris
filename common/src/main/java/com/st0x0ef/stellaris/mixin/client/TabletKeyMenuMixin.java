5d517df978cbacf8798484e93943a143ea2a2bb9
TATHAN <costa.tathan@gmail.com>
2025-03-09 16:05:31 +0100

    Fix tablet info bar

--- common/src/main/java/com/st0x0ef/stellaris/mixin/client/TabletKeyMenuMixin.java
+++ common/src/main/java/com/st0x0ef/stellaris/mixin/client/TabletKeyMenuMixin.java
@@ -1,14 +1,15 @@
 package com.st0x0ef.stellaris.mixin.client;
 
-import com.st0x0ef.stellaris.Stellaris;
 import com.st0x0ef.stellaris.client.events.ClientEvents;
 import com.st0x0ef.stellaris.client.registries.KeyMappingsRegistry;
 import com.st0x0ef.stellaris.common.network.packets.OpenTabletEntryPacket;
 import dev.architectury.networking.NetworkManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.MenuScreens;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -17,6 +18,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Screen.class)
 public class TabletKeyMenuMixin {
 
+    @Unique
+    private boolean stellaris$isHolding = false;
+
+    @Inject(at = @At("HEAD"), method = "tick")
+    private void onTabletKeyPressed(CallbackInfo ci) {
+        if (!stellaris$isHolding) {
+            if (ClientEvents.timeClicked > 0) ClientEvents.timeClicked--;
+        } else {
+            stellaris$isHolding = false;
+        }
+
+    }
+
     @Inject(at = @At("HEAD"), method = "keyPressed")
     private void onTabletKeyPressed(int keyCode, int scanCode, int modifiers, CallbackInfoReturnable<Boolean> cir) {
         Player player = Minecraft.getInstance().player;
@@ -24,6 +38,7 @@ public class TabletKeyMenuMixin {
             return;
         }
         if (KeyMappingsRegistry.OPEN_TABLET_INFO.key.getValue() == keyCode) {
+            stellaris$isHolding = true;
             if(ClientEvents.entryHovered != null) {
                 ClientEvents.timeClicked++;
                 if(ClientEvents.timeClicked == 30) {
@@ -33,14 +48,17 @@ public class TabletKeyMenuMixin {
                 }
             }
         } else {
-            if (ClientEvents.timeClicked > 0) ClientEvents.timeClicked--;
+            stellaris$isHolding = false;
         }
     }
+
+
 
     @Inject(at = @At("HEAD"), method = "onClose")
     private void onScreenClose(CallbackInfo ci) {
         ClientEvents.timeClicked = 0;
         ClientEvents.entryHovered = null;
+        stellaris$isHolding = false;
     }
 
 }
