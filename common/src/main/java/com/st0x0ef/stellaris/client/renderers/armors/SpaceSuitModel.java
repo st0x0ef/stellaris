df6ae4c7100af9e1abfa9fc12b59adb4afd481f4
eirmax <eirmax8@gmail.com>
2025-03-24 11:25:39 +0700

    notnull body parts

--- common/src/main/java/com/st0x0ef/stellaris/client/renderers/armors/SpaceSuitModel.java
+++ common/src/main/java/com/st0x0ef/stellaris/client/renderers/armors/SpaceSuitModel.java
@@ -17,6 +17,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class SpaceSuitModel extends HumanoidModel<LivingEntity> {
@@ -110,12 +111,12 @@ public class SpaceSuitModel extends HumanoidModel<LivingEntity> {
 	}
 
 	@Override
-	protected Iterable<ModelPart> headParts() {
+	protected @NotNull Iterable<ModelPart> headParts() {
 		return ImmutableList.of(head);
 	}
 
 	@Override
-	protected Iterable<ModelPart> bodyParts() {
+	protected @NotNull Iterable<ModelPart> bodyParts() {
 		return ImmutableList.of(body, rightArm, leftArm, rightLeg, leftLeg, hat);
 	}
 
