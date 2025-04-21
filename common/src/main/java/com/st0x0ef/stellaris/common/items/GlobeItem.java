d372c5c62ddb215c19ec179b866e53388c97c3b3
st0x0ef <gabriel.gaudreault3@gmail.com>
2025-04-20 20:25:56 -0400

    fix globe

--- common/src/main/java/com/st0x0ef/stellaris/common/items/GlobeItem.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/items/GlobeItem.java
@@ -6,15 +6,19 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.Block;
 
 public class GlobeItem extends BlockItem implements CustomTabletEntry {
+    private final ResourceLocation texture;
 
-    public GlobeItem(Block block, Properties properties) {
+    public GlobeItem(Block block, ResourceLocation texture, Properties properties) {
         super(block, properties);
+        this.texture = texture;
+    }
+
+    public ResourceLocation getTexture() {
+        return this.texture;
     }
 
     @Override
     public ResourceLocation getEntryName(ItemStack stack) {
         return ResourceLocation.fromNamespaceAndPath("planets", this.arch$registryName().getPath().replace("_globe", ""));
     }
-
-
 }
