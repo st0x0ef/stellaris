cb105f8f5f372a81f7e27c42fc52de2fffbd2a0c
Fej1Dev <101066376+Fej1Dev@users.noreply.github.com>
2025-05-25 22:57:09 +0300

    Register JEI integration in neoforge

---
+++ neoforge/src/main/java/com/st0x0ef/stellaris/neoforge/compats/NeoForgeJEICompat.java
@@ -0,0 +1,9 @@
+package com.st0x0ef.stellaris.neoforge.compats;
+
+import com.st0x0ef.stellaris.common.compats.jei.JEIPlugin;
+import mezz.jei.api.JeiPlugin;
+
+@JeiPlugin
+public class NeoForgeJEICompat extends JEIPlugin {
+
+}
