7d69a5cca082ff383c98407f72c4c2fba511b43a
Fej1Dev <101066376+Fej1Dev@users.noreply.github.com>
2025-12-01 00:33:50 +0200

    Fix #167, #166 and #165

--- common/src/main/java/com/st0x0ef/stellaris/common/blocks/UpgradeStationBlock.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/blocks/UpgradeStationBlock.java
@@ -4,7 +4,9 @@ import com.mojang.serialization.MapCodec;
 import com.st0x0ef.stellaris.common.menus.UpgradeStationMenu;
 import dev.architectury.registry.menu.ExtendedMenuProvider;
 import dev.architectury.registry.menu.MenuRegistry;
+import it.unimi.dsi.fastutil.doubles.DoubleList;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
@@ -13,10 +15,17 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.CubeVoxelShape;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class UpgradeStationBlock extends Block {
@@ -27,6 +36,7 @@ public class UpgradeStationBlock extends Block {
 
     public static final MapCodec<UpgradeStationBlock> CODEC = simpleCodec(UpgradeStationBlock::new);
     private static final Component CONTAINER_TITLE = Component.translatable("container.stellaris.upgrade");
+    private static final VoxelShape SHAPE = Block.box(0,0,0,16,12,16);
 
     @Override
     public MapCodec<UpgradeStationBlock> codec() {
@@ -62,5 +72,10 @@ public class UpgradeStationBlock extends Block {
             MenuRegistry.openExtendedMenu((ServerPlayer) player, this.getMenuProvider(state, level, pos));
             return InteractionResult.CONSUME;
         }
+    }
+
+    @Override
+    protected @NotNull VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return SHAPE;
     }
 }
